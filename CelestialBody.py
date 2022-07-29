851aaf2aacc5a6c5973ca34a1c685c0d3657a38e
Tigran Tadevosyan <Tigran Tadevosyan>
2022-07-29 22:19:42 +0400

    Constructed motion

--- CelestialBody.py
+++ CelestialBody.py
@@ -1,5 +1,8 @@
 import pygame.math as math
 
+G_const = 6.67 * pow(10, -11)
+iteration_count = 500
+
 class CelestialBody:
     def __init__(self, name, color, mass, radius, velocity, position):
         self.name = name
@@ -8,16 +11,48 @@ class CelestialBody:
         self.radius = radius
         self.velocity = velocity
         self.position = position
+        
 
-Sun = CelestialBody("Sun", (0, 125, 125), 10, 50, math.Vector2(100, 100), math.Vector2(400, 400))
-Mercury = CelestialBody("Mercury", (0, 125, 125), 10, 50, math.Vector2(100, 100), math.Vector2(400, 400))
-Venus = CelestialBody("Venus", (0, 125, 125), 10, 50, math.Vector2(100, 100), math.Vector2(400, 400))
-Earth = CelestialBody("Earth", (0, 125, 125), 10, 50, math.Vector2(100, 100), math.Vector2(400, 400))
-Mars = CelestialBody("Mars", (0, 125, 125), 10, 50, math.Vector2(100, 100), math.Vector2(400, 400))
-Jupiter = CelestialBody("Jupiter", (0, 125, 125), 10, 50, math.Vector2(100, 100), math.Vector2(400, 400))
-Saturn = CelestialBody("Saturn", (0, 125, 125), 10, 50, math.Vector2(100, 100), math.Vector2(400, 400))
-Uranus = CelestialBody("Uranus", (0, 125, 125), 10, 50, math.Vector2(100, 100), math.Vector2(400, 400))
-Neptun = CelestialBody("Neptun", (0, 125, 125), 10, 50, math.Vector2(100, 100), math.Vector2(400, 400))
+    def update(self, millisec, Bodies, speed):
+        if self.mass == 0:
+            return
+        
+        dt =  millisec * speed / iteration_count
+        for i in range(1, iteration_count):
+            a = math.Vector2(0, 0)
+            self.position += dt * self.velocity
 
-Bodies = [Sun, Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptun]
+            for body in Bodies:
+                if body.name == self.name:
+                    continue
+
+                self2BodyVec = self.position - body.position
+                a += -1 * self2BodyVec.normalize() * G_const * body.mass/self2BodyVec.length_squared()
+            
+            self.velocity += dt * a
+        
+
+Sun = CelestialBody("Sun", (235, 211, 0), 1.989* pow(10, 30), 5,
+                    math.Vector2(0, 0), math.Vector2(0, 0))
+Mercury = CelestialBody("Mercury", (235, 176, 100), 3.3 * pow(10, 23), 2,
+                        math.Vector2(47870, 0), math.Vector2(0, 58 * pow(10, 9)))
+Venus = CelestialBody("Venus", (235, 188, 57), 48.67*pow(10, 23), 9,
+                      math.Vector2(35020, 0), math.Vector2(0, 108 * pow(10, 9)))
+Earth = CelestialBody("Earth", (8, 173, 135), 59.72*pow(10, 23), 5,
+                      math.Vector2(29780, 0), math.Vector2(0, 150 * pow(10, 9)))
+Mars = CelestialBody("Mars", (219, 93, 237), 6.417*pow(10, 23), 2.5,
+                     math.Vector2(24077, 0), math.Vector2(0, 228 * pow(10, 9)))
+Jupiter = CelestialBody("Jupiter", (125, 0, 125), 18990*pow(10, 23), 40,
+                        math.Vector2(13070, 0), math.Vector2(0, 778 * pow(10, 9)))
+Saturn = CelestialBody("Saturn", (200, 200, 200), 5685*pow(10, 23), 30,
+                       math.Vector2(9690, 0), math.Vector2(0, 1400 * pow(10, 9)))
+Uranus = CelestialBody("Uranus", (51, 200, 125), 868.2*pow(10, 23), 15,
+                       math.Vector2(6810, 0), math.Vector2(0, 2900 * pow(10, 9) ))
+Neptun = CelestialBody("Neptun", (0, 200, 0), 1024*pow(10, 23), 13,
+                       math.Vector2(5430, 0), math.Vector2(0, 4500 * pow(10, 9)))
+
+CustomBody = CelestialBody("", (255, 0, 0), 0, 10,
+                           math.Vector2(0, 0), math.Vector2(0, -5000 * pow(10, 9)))
+
+Bodies = [CustomBody, Sun, Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptun]
 
--- main.py
+++ main.py
@@ -1,32 +1,75 @@
 import pygame
+import pygame.math as math
 
 import pygame_widgets
+from pygame_widgets.button import Button
+from pygame_widgets.toggle import Toggle
 from pygame_widgets.slider import Slider
 from pygame_widgets.textbox import TextBox
 
+import numpy as np
+
 from CelestialBody import Bodies
+from CelestialBody import CustomBody
 
 pygame.init()
 
-Window = pygame.display.set_mode([1500, 1200])
+frameWidth = 1500
+frameHeight = 1200
+frameSize = math.Vector2(1500, 1200)
 
-mass_slider =     Slider(Window, 50, 25, 100, 10, min=0, max=100, step=1)
-angle_slider =    Slider(Window, 50, 75, 100, 10, min=-90, max=90, step=1)
-velocity_slider = Slider(Window, 50, 125, 100, 10, min=0, max=100, step=1)
+Window = pygame.display.set_mode(frameSize)
 
-mass_output =     TextBox(Window, 175, 15, 150, 30, fontSize=25)
-angle_output =    TextBox(Window, 175, 65, 150, 30, fontSize=25)
-velocity_output = TextBox(Window, 175, 115, 150, 30, fontSize=25)
+mass_slider =     Slider(Window, 1050, 25, 100, 10, min=0, max=1000000, step=1, initial=1000)
+angle_slider =    Slider(Window, 1050, 75, 100, 10, min=-90, max=90, step=1)
+velocity_slider = Slider(Window, 1050, 125, 100, 10, min=0, max=100000, step=1)
+scale_slider =    Slider(Window, 1050, 175, 100, 10, min=500000000, max=10000000000, step=1, initial=3000000000)
+speed_slider =    Slider(Window, 1050, 225, 100, 10, min=1000, max=400000, step=1, initial=6000)
 
+mass_output =     TextBox(Window, 1175, 15, 150, 30, fontSize=25)
+angle_output =    TextBox(Window, 1175, 65, 150, 30, fontSize=25)
+velocity_output = TextBox(Window, 1175, 115, 150, 30, fontSize=25)
+scale_output =    TextBox(Window, 1175, 165, 150, 30, fontSize=25)
+speed_output =    TextBox(Window, 1175, 215, 150, 30, fontSize=25)
+daysPassed_output=TextBox(Window, 1050, 265, 300, 30, fontSize=25)
+
+showNamesLabel = TextBox(Window, 1050, 300, 300, 30, fontSize=25)
+showNamesLabel.setText("Show planet names: ")
+showNamesToggle = Toggle(Window, 1240, 307, 40, 15, startOn = True)
+
+staticSunLabel = TextBox(Window, 1050, 335, 300, 30, fontSize=25)
+staticSunLabel.setText("Static Sun: ")
+staticSunToggle = Toggle(Window, 1240, 342, 40, 15, startOn = False)
+
+font = pygame.font.SysFont('Comic Sans MS', 15)
+
+def addCustomBody():
+    CustomBody.position = math.Vector2(0, -5000 * pow(10, 9))
+    
+    CustomBody.mass = mass_slider.getValue() * pow(10, 20)
+
+    theta = np.deg2rad(angle_slider.getValue())
+    CustomBody.velocity = velocity_slider.getValue() * math.Vector2(np.sin(theta), np.cos(theta))
+    
+startButton = Button(Window, 1150, 375, 100, 25, text='Start', fontSize=25, margin=5, radius=20, onClick= addCustomBody)
+    
+def drawText(text, Window, x, y):
+    textsurface = font.render(text, False, (0, 0, 0))
+    Window.blit(textsurface, (x, y))
 
 def drawBodies(Bodies, Window):
     for body in Bodies:
-        pygame.draw.circle(Window, body.color, body.position, body.radius)
-
-
+        pygame.draw.circle(Window, body.color, frameSize/2+body.position/scale_slider.getValue(), body.radius)
+    if showNamesToggle.getValue():
+        for body in Bodies:    
+            drawText(body.name, Window, frameSize.x/2+body.position.x/scale_slider.getValue(), frameSize.y/2+body.position.y/scale_slider.getValue())
 
 
 running = True
+clock = pygame.time.Clock()
+fps = 30
+
+days_passed = 0
 
 while running:
     events = pygame.event.get()
@@ -36,18 +79,31 @@ while running:
 
     Window.fill((255, 255, 255))
 
-    #pygame.draw.circle(Window, (0, 0, 255), (1000, 500), 250)
-
+    tick = clock.tick(50)
+    
+    for body in Bodies:
+        if staticSunToggle.getValue() and body.name == "Sun":
+            continue
+        
+        body.update(tick, Bodies, speed_slider.getValue())
+    
     drawBodies(Bodies, Window)
     
-    mass_output.setText("Mass: %d X 10^10 kg" % mass_slider.getValue())
+    mass_output.setText("Mass: %d kg" % mass_slider.getValue())
     angle_output.setText("Angle: %d deg" % angle_slider.getValue())
-    velocity_output.setText("Velocity: %d km/s" % velocity_slider.getValue())
+    velocity_output.setText("Velocity: %d m/s" % velocity_slider.getValue())
+    scale_output.setText("Scale: %.1f bilion m/p" % (scale_slider.getValue()/pow(10,9)))
+    
+    speed = speed_slider.getValue()*1000/3600 # hours per second
+    speed_output.setText("Speed: %d h/s" % speed)
+
+    days_passed += speed*tick/24000
+    daysPassed_output.setText("%d earth days have passed" % days_passed)
     
     # Flip the display
-    pygame_widgets.update(events)
+    pygame_widgets.update(events) 
     pygame.display.update()
 
 # Done! Time to quit.
 
-pygame.quit()
+pygame.quit()                   
